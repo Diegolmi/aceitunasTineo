9d48c2ba22c1dc5f679d6725d4525dd599f704fb
Diego <diegolmi@hotmail.com>
2020-09-15 21:50:07 -0300

    Imagenes de portada

--- src/Pages/HomePage/GaleriaImagenes.js
+++ src/Pages/HomePage/GaleriaImagenes.js
@@ -1,5 +1,8 @@
 import React from "react";
-import img1 from "../../assets/img/PNG/DoyPackChico/Verdex100.jpg";
+import Descarozadax100 from "../../assets/img/PNG/DoyPackChico/Descarozadax100.jpg";
+import Picklex180 from "../../assets/img/PNG/DoyPackGrande/Picklex180.jpg";
+import AjiFrasco from "../../assets/img/PNG/Frascos/Frasco_ají.png";
+import VerdeSachet from "../../assets/img/PNG/Sachet/VerdeSachet.jpg";
 
 
 const GaleriaImagenes = () => {
@@ -8,28 +11,28 @@ const GaleriaImagenes = () => {
       <h1 className="titulo-about">Nuestros Productos</h1>
       <div className="contenedor-imagenes">
         <div className="imagen">
-          <img src={img1} alt="" />
+          <img src={Descarozadax100} alt="" />
           <div className="overlay3">
           <h4>Pack Chico</h4>
             <a  href="/productos/packchico" className="button">Ver Más</a>
           </div>
         </div>
         <div className="imagen">
-          <img src={img1} alt="" />
+          <img src={Picklex180} alt="" />
           <div className="overlay3">
           <h4>Pack Grande</h4>
             <a href="/productos/packgrande" className="button">Ver Más</a>
           </div>
         </div>
         <div className="imagen">
-          <img src={img1} alt="" />
+          <img src={VerdeSachet} alt="" />
           <div className="overlay3">
           <h4>Sachet</h4>
             <a href="/productos/sachet" className="button">Ver Más</a>
           </div>
         </div>
         <div className="imagen">
-          <img src={img1} alt="" />
+          <img src={AjiFrasco} alt="" />
           <div className="overlay3">
           <h4>Frascos</h4>
             <a href="/productos/frasco" className="button">Ver Más</a>
