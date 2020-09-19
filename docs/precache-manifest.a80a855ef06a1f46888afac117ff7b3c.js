c125bceb0d82c60842a3030a728774ae8f55163b
Diego <diegolmi@hotmail.com>
2020-09-19 00:51:10 -0300

    cambio build

---
+++ docs/precache-manifest.a80a855ef06a1f46888afac117ff7b3c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb798eaa7ad5b6e7bc5397699bfe438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8a30ed847b4e63008f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f43a0c2282088905f0e7",
+    "url": "/static/css/main.a4be823c.chunk.css"
+  },
+  {
+    "revision": "ca8a30ed847b4e63008f",
+    "url": "/static/js/2.1bbfe5b3.chunk.js"
+  },
+  {
+    "revision": "f6a33b08b8dab27dd8ab525dc9994832",
+    "url": "/static/js/2.1bbfe5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f43a0c2282088905f0e7",
+    "url": "/static/js/main.b20628cb.chunk.js"
+  },
+  {
+    "revision": "3398892089ddce62928f",
+    "url": "/static/js/runtime-main.da92e66c.js"
+  },
+  {
+    "revision": "864254fa8a805a78fb2022d65a5965d3",
+    "url": "/static/media/Coberturax100.864254fa.jpg"
+  },
+  {
+    "revision": "bff839c7be6002c1065ad59987010c0e",
+    "url": "/static/media/DescarozadaSachet.bff839c7.jpg"
+  },
+  {
+    "revision": "5fae4bdb56fc916e2b74f5b8b0851d2e",
+    "url": "/static/media/Descarozadax100.5fae4bdb.jpg"
+  },
+  {
+    "revision": "e69f97afcfc6f73f25f4d6722808c242",
+    "url": "/static/media/Descarozadax160.e69f97af.jpg"
+  },
+  {
+    "revision": "529a002d07cb46c3cdbfd98c6dc0426f",
+    "url": "/static/media/FontsFree-Net-Homework.529a002d.ttf"
+  },
+  {
+    "revision": "89d666aad543e3f6334b04ee4280789c",
+    "url": "/static/media/Frasco_Verde.89d666aa.png"
+  },
+  {
+    "revision": "cb5171839ab622d96ee4be44a065e53d",
+    "url": "/static/media/Frasco_ají.cb517183.png"
+  },
+  {
+    "revision": "425f5929046454f02ef8648af807f319",
+    "url": "/static/media/Frasco_descarozada.425f5929.png"
+  },
+  {
+    "revision": "2b0f87731fa5a7fbf471800ffdbff9f8",
+    "url": "/static/media/Frasco_negra.2b0f8773.png"
+  },
+  {
+    "revision": "b883194b887bc135bd338ade2f1e9a21",
+    "url": "/static/media/Frasco_pepinillo.b883194b.png"
+  },
+  {
+    "revision": "d212c65fbfea2e6336cae638684c4d4e",
+    "url": "/static/media/Frasco_rodajada.d212c65f.png"
+  },
+  {
+    "revision": "ec5ba11289051958a1c1b5eed0e88624",
+    "url": "/static/media/Negrax180.ec5ba112.jpg"
+  },
+  {
+    "revision": "3565983d71d118b1d827052f2d938fcb",
+    "url": "/static/media/PORTADA_Mesa-de-trabajo1.3565983d.png"
+  },
+  {
+    "revision": "e2791186c0e189ca1ca8021edc3a5b64",
+    "url": "/static/media/Pepinillox100.e2791186.jpg"
+  },
+  {
+    "revision": "cb818962427e2361371cd27a9f37f156",
+    "url": "/static/media/Picklex180.cb818962.jpg"
+  },
+  {
+    "revision": "e7f249e71a563eed9c495835657eb9c2",
+    "url": "/static/media/PoetsenOne-Regular.e7f249e7.ttf"
+  },
+  {
+    "revision": "e43b7b233a49b05caa7109f262caea2f",
+    "url": "/static/media/Premiumx100.e43b7b23.jpg"
+  },
+  {
+    "revision": "295a7f826b788265b50b58591e1e4d53",
+    "url": "/static/media/Premiumx180.295a7f82.jpg"
+  },
+  {
+    "revision": "72829737823e78da9933673404da75f7",
+    "url": "/static/media/RellenaSachet.72829737.jpg"
+  },
+  {
+    "revision": "ff0554481574598e7533fdc72c1ce332",
+    "url": "/static/media/Rellenax100.ff055448.jpg"
+  },
+  {
+    "revision": "2063e9a87c4b5b4410522194cf523dae",
+    "url": "/static/media/Rellenax180.2063e9a8.jpg"
+  },
+  {
+    "revision": "11dfe40626f34ee1b6fe60b1e4944223",
+    "url": "/static/media/Rodajadax100.11dfe406.jpg"
+  },
+  {
+    "revision": "f3415d7e4a91a0e74fe2ee4eb73c3662",
+    "url": "/static/media/Rodajadax160.f3415d7e.jpg"
+  },
+  {
+    "revision": "34dec8372c344c49824bfb7165597698",
+    "url": "/static/media/VerdeSachet.34dec837.jpg"
+  },
+  {
+    "revision": "458e4566c2c82a2f6126ae151c70e10a",
+    "url": "/static/media/Verdex100.458e4566.jpg"
+  },
+  {
+    "revision": "7177174c1195f8a2e0478042678cd90f",
+    "url": "/static/media/Verdex180.7177174c.jpg"
+  },
+  {
+    "revision": "811347129415cc15db913c951b20deed",
+    "url": "/static/media/logoTineo.81134712.png"
+  }
+]);
